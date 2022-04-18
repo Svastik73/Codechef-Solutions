63b2f21a413ff963609726fe81191ad55ff9bee2
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-18 19:13:39 +0530

    Create Mario and Transformation(TRANSFORM).cpp

---
+++ Mario and Transformation(TRANSFORM).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+      int n; cin>>n;
+    if(n%3==0) cout<<"NORMAL\n";
+    else if((n-1)%3==0) cout<<"HUGE\n";
+    else cout<<"SMALL\n";
+
+
+
+
+
+
+
+    }
+
+}
