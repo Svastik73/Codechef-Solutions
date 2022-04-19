7ce5fefaca395623214e8dc60d0d6cc9fd070a12
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-19 16:10:11 +0530

    Create Chef and Chocolates(CHEFCHOCO).cpp

---
+++ BEGINNER/Chef and Chocolates(CHEFCHOCO).cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+ 
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+    int c,x,y;  cin>>c>>x>>y;
+    cout<<(c-x)*y<<endl;
+        
+        
+        
+        
+        
+        
+    }
+}
