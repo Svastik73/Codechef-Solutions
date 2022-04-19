3a88d2c42395462b0c5ed77cb1a4f73111123a7d
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-19 16:08:51 +0530

    Create Badminton Serves(BADMINTON).cpp

---
+++ BEGINNER/Badminton Serves(BADMINTON).cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    
+ int t; scanf("%d",&t);
+ while(t--){
+  int n; scanf("%d",&n);
+  printf("%d\n",n/2+1);
+ }
+    
+
+    }
