bba977a498a07672359d8c454c26fe36106c5d2b
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-19 16:03:55 +0530

    Create Credit score(CREDSCORE).cpp

---
+++ BEGINNER/Credit score(CREDSCORE).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+
+int main(){
+  int n; cin>>n;
+  if(n>=750){
+    cout<<"YES";
+  }
+  else
+  {
+    cout<<"NO";
+    
+  }
+}
+
+
+
+
