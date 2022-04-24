a6d5964f2d239ee5a50118939b00293ae37fdc57
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-24 21:54:32 +0530

    Create Can You Eat It(COMPCAND).cpp

---
+++ BEGINNER/Can You Eat It(COMPCAND).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+
+using namespace std;
+int main(){
+int t; cin>>t;
+while(t--){
+    int n,k;
+    cin>>n>>k;
+    if(n%k==0){
+        cout<<n/k<<endl;
+        
+    }
+    else 
+    {
+        cout<<-1<<endl;
+    }
+    
+    
+    
+    
+    
+
+}
+
+}
