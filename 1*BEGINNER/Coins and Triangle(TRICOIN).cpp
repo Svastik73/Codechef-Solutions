dce6efa0649b9bf8840535896e820ef9faa4f0ff
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-28 19:29:29 +0530

    Create Coins and Triangle(TRICOIN).cpp

---
+++ 1*BEGINNER/Coins and Triangle(TRICOIN).cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int main(){
+
+int t; cin>>t;
+
+while(t--){
+    
+    int a; cin>>a;
+    cout<<(int)(-1+sqrt(1+4*2*a))/2<<endl;
+    //Type casting USA!!USA!!
+    
+
+
+
+
+}
+
+}
