9e876a99727783cb12ac4ec040a93c8fb5f3ab02
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-30 19:47:58 +0530

    Create Food Chain(FODCHAIN).cpp

---
+++ 1*BEGINNER/Food Chain(FODCHAIN).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cmath>               //USA USA
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+
+int t; cin>>t;
+while(t--){
+ int n,x; cin>>n>>x;
+ int level=1;
+ while(n/x>0){
+  n=n/x;
+  level++;
+  
+  
+ }
+ cout<<level<<endl;
+ 
+}
+
+
+ 
+ 
+ 
+      }
