fe660d327f74b75a7fee366817d7ee0e23b3f677
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-05-01 15:38:38 +0530

    Create Is The Score Consistent(TRUESCORE).cpp

---
+++ BEGINNER/Is The Score Consistent(TRUESCORE).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	                     //^_^
+                        //******--------
+                        //******---------
+                        //******---------
+	int t;                //---------------
+	cin>>t;               //---------------
+	while(t--){         //USA! USA! USA!
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if(c<a or d<b) cout<<"IMPOSSIBLE"<<endl;
+	    else cout<<"POSSIBLE"<<endl;
+	}
+	return 0;
+}
