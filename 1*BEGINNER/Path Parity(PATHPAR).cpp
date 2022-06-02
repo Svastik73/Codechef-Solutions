d498aa7e8b5416888a0f3f8d7db87cfc64576c26
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-06-02 18:34:49 +0530

    Create Path Parity(PATHPAR).cpp

---
+++ 1*BEGINNER/Path Parity(PATHPAR).cpp
@@ -0,0 +1,15 @@
+\\USA USA AMERICA!!!! 
+#include <iostream>
+using namespace std;
+
+int main(){
+	int t; cin>>t;
+	while(t--){
+	    int x,y; cin>>x>>y;
+	    if(x%2==0) cout<<"yes\n";
+	    else if(y==0) cout<<"no\n";
+	    
+	    else if(y==1) cout<<"yes\n";
+	    
+	 	}
+}
